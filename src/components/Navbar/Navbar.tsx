fb74fd05e13d8fc137785a7e2b34d68d796ff25d
Murtaza Ahmad <murtaza.ahmad0075@gmail.com>
2024-07-21 17:02:40 +0500

    feat: Add New Component Files

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    return (
+        <>
+            {/* Todos:
+            1. Create Navbar as per PSD
+            2. Make it responsive
+            */}
+        </>
+    )
+}
+
+export default Navbar
