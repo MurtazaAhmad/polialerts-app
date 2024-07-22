6fdf8e41f16397b852d4b2786784cebc082d99e3
Murtaza Ahmad <murtaza.ahmad0075@gmail.com>
2024-07-22 12:02:38 +0500

    Revert "Login Page added along with navbar and footer"
    This reverts commit 3fbaca4413467f708324d9a453cf76805718a241.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Footer from "@/components/Footer/Footer";
-import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-   <p>home</p>
+      <p>helo</p>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,20 +13,6 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
-        headingColor: "#0c0c0c",
-        bodyColor: "#515151",
-        blueColor: "#2468cd",
-        blueHover: "#3b4352",
-        lightGray: "#ececec",
-        darkGray: "#3b4352",
-        white: "#ffffff",
-        iota:'#6f6f6f'
-
-      },
-      fontFamily: {
-        "Manrope": ["Manrope", "sans-serif"],      
-      }
     },
   },
   plugins: [],
