865c5c14085f346c6b09b38c588b6ff227ddbbe2
Murtaza Ahmad <murtaza.ahmad0075@gmail.com>
2024-07-27 12:10:46 +0500

    feat: Add basic query to fetch data
    - Add firebase logic to fetch government_levels.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { useCollection } from 'react-firebase-hooks/firestore'
+
+import { db } from '../firebase/config'
+import { collection, addDoc, Timestamp, query, orderBy, onSnapshot } from 'firebase/firestore'
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
+
+    const [value, loading, error] = useCollection(collection(db, 'government_levels'))
+    const governmentLevels = value?.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+    }))
+
+    console.log("governmentLevels2", governmentLevels);
+
     return (
         <div>page</div>
     )
