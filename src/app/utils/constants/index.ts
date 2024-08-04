c57b85465dce35fa843e4361e0eb141983c026d7
Murtaza Ahmad <murtaza.ahmad0075@gmail.com>
2024-08-04 13:44:38 +0500

    feat: Add constants
    - Add file for Collection Names

---
+++ src/app/utils/constants/index.ts
@@ -0,0 +1,4 @@
+export const USERS_COLLECTION = "Users";
+export const KEYWORDS_COLLECTION = "Keywords";
+export const STREAMING_SOURCE_COLLECTION = "StreamingSource";
+export const CATEGORY_COLLECTION = "Category";
