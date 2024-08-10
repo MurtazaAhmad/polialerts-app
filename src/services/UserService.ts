18ceb65fd32b0d243eefd79c469ae60770c599ad
Murtaza Ahmad <murtaza.ahmad0075@gmail.com>
2024-08-10 12:12:38 +0500

    Add User service

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,2 +1,14 @@
+import { UserRepository } from "@/repositories/UserRepository";
+import { User } from "@/types";
+
 export class UserService {
+    private readonly userRepository: UserRepository;
+
+    constructor(userRepository: UserRepository) {
+        this.userRepository = userRepository;
+    }
+
+    async getUsers(): Promise<User[]> {
+        return this.userRepository.getUsers();
+      }
 }
